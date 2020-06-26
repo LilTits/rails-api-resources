e584f638d923830db98552013b738e81c4977c2e
LilTits <22703571+LilTits@users.noreply.github.com>
2020-06-26 11:14:39 +0200

    Run the migration and seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+restaurants = []
+1000.times do
+  restaurants << Restaurant
+  .create!({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address
+  })
+end
+
+restaurants.each do |restaurant|
+  2.times do
+    restaurant.dishes
+    .create({
+      name: Faker::Food.dish,
+      rating: Faker::Number.between(from: 0, to: 5)
+    })
+  end
+end
