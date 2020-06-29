f3cc09b70639fb02015f6b8495ca43e09176f30b
LilTits <22703571+LilTits@users.noreply.github.com>
2020-06-29 16:47:49 +0200

    Configure jsonapi

---
+++ config/initializers/jsonapi_resources.rb
@@ -0,0 +1,8 @@
+JSONAPI.configure do |config|
+  config.default_caching = true
+
+  config.default_paginator = :paged # default is :none
+
+  config.default_page_size = 50 # default is 10
+  config.maximum_page_size = 100 # default is 20 
+end
